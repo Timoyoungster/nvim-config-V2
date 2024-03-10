4e05b53a64dcf1199bfdb80cd9f7ebc0eb733a67
Timoyoungster <53046195+Timoyoungster@users.noreply.github.com>
2024-03-10 20:35:49 +0100

    feat: rustfmt 2 space indent

--- plugins.lua
+++ plugins.lua
@@ -390,7 +390,14 @@ require('lazy').setup({
         pyright = {
 
         },
-        rust_analyzer = {},
+        rust_analyzer = {
+          rustfmt = {
+            extraArgs = {
+              "--config",
+              "tab_spaces=2",
+            },
+          },
+        },
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
