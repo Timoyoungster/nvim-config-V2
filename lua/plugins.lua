f6f3db8b7529a7297093d616fd3d80fa71e00f4d
Timoyoungster <timo.proemer04@gmail.com>
2024-04-19 12:42:41 +0200

    feat: rose-pine theme again

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -432,20 +432,18 @@ require('lazy').setup({
     end,
   },
 
+  --   'folke/tokyonight.nvim',
+  --   'rebelot/kanagawa.nvim',
   { -- colorscheme
-    'folke/tokyonight.nvim',
+    'rose-pine/neovim',
+    name = 'rose-pine',
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
-
-      -- You can configure highlights by doing something like
-      vim.cmd.hi 'Comment gui=none'
+      vim.cmd.colorscheme 'rose-pine'
     end,
   },
+
 
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
@@ -531,6 +529,6 @@ require('lazy').setup({
     },
   },
   install = {
-    colorscheme = { "tokyonight-night" }
+    colorscheme = { "rose-pine" }
   },
 })
