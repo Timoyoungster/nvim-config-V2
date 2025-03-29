92f53ed2371f83c622d1c0dae48fafb55571dcfc
Timoyoungster <timo.proemer04@gmail.com>
2025-03-29 21:36:53 +0100

    feat: keybind for markdown preview

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -86,10 +86,13 @@ require('lazy').setup({
   },
 
   { -- markdown previewer
-      "iamcco/markdown-preview.nvim",
-      cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-      ft = { "markdown" },
-      build = ':call mkdp#util#install()'
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = ':call mkdp#util#install()',
+    config = function()
+      vim.keymap.set("n", "gm", ":MarkdownPreviewToggle")
+    end,
   },
 
   { -- university
